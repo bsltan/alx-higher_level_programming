71c393e9d02532f7aa96d320a5f96cc8a3deb1c4
bsltan <califchichi@yahoo.com>
2023-01-24 12:54:52 +0300

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,20 +1,58 @@
 #!/usr/bin/python3
-"""Defines a square-printing function."""
+# -*- coding: utf-8 -*-
+"""This is a class for a square with attribute size"""
 
 
-def print_square(size):
-    """Print a square with the # character.
-    Args:
-        size (int): The height/width of the square.
-    Raises:
-        TypeError: If size is not an integer.
-        ValueError: If size is < 0
-    """
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
+class Square:
+    """This is a square class """
 
-    for i in range(size):
-        [print("#", end="") for j in range(size)]
-        print("")
+    def __init__(self, size=0):
+        """
+        Constructs all the necessary attributes for square
+        Parameters
+        ----------
+        size : int
+            the size of square object
+        """
+        try:
+            self.__size = size
+            if not isinstance(size, int):
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+        except Exception as exception:
+            raise exception
+
+    @property
+    def size(self):
+        """
+        gets the current sqare size
+        Returns:
+            The size of the current object
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        setter method for size
+        Args:
+            value : int
+            the size of square object
+        """
+        try:
+            self.__size = value
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+        except Exception as exception:
+            raise exception
+
+    def area(self):
+        """
+        Calculates the current sqare area
+        Returns:
+            The area of the current object
+        """
+        return self.__size * self.__size
