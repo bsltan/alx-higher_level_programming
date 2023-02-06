48fd54c65a46148d057d13e0a19f40088a5266f9
bsltan <califchichi@yahoo.com>
2023-02-06 19:18:59 +0300

    Python - Inheritance

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,11 +5,11 @@ Creates a class inheriting from the list class.
 
 
 class MyList(list):
-        """Class MyList inherits from list."""
+    """Class MyList inherits from list."""
 
-            def print_sorted(self):
-                        """Prints the list, in ascending sort."""
+    def print_sorted(self):
+        """Prints the list, in ascending sort."""
 
-                                new_list = self[:]
-                                        new_list.sort()
-                                                print("{}".format(new_list))
+        new_list = self[:]
+        new_list.sort()
+        print("{}".format(new_list))
--- 0x0A-python-inheritance/README.md
+++ 0x0A-python-inheritance/README.md
@@ -31,4 +31,3 @@ Files | Description
 - [Inheritance](https://docs.python.org/3/tutorial/classes.html#inheritance)
 - [Inheritance in Python](https://www.packt.com/inheritance-python/)
 - [Learn to Program 10 : Inheritance Magic Methods](https://www.youtube.com/watch?v=d8kCdLCi6Lk)
-
