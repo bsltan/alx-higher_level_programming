1e759a762b150960ae1523cd310a41239f2a73e9
bsltan <califchichi@yahoo.com>
2023-05-23 08:46:16 +0300

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-
 const request = require('request');
+const { argv } = require('process');
 
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
+const BaseUrl = 'https://swapi-api.alx-tools.com/api';
+request(BaseUrl + '/films/' + argv[2], (error, response, body) => {
   if (error) {
     console.error(error);
   }
   console.log(JSON.parse(body).title);
-});
+})
